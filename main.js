49b12d53ec10b484fb10098b627e13543515097f
ReichertA2 <aydee0112@yahoo.com>
2022-05-24 15:52:03 -0500

    more css styling

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ function handleSubmit(event){
     event.stopPropagation();
     event.preventDefault();
     console.log(document.getElementsByName("city")[0]. value) 
-    
+
+  
     doAPICall(document.getElementsByName("city")[0]. value)
+
 
 }
 
@@ -14,23 +16,62 @@ function handleSubmit(event){
 async function doAPICall(city){
     let result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=imperial`)
     console.log(result)
+
+
+   
+    let img = document.createElement("img")
+    img.src = "images/clouds.jpg";
+    img.class = "card-img-overlay";
+    img.alt = "...";
+    img.style.height = "450px"
+    img.style.width = "290px"
+    img.style.marginBottom = "350px";
+    document.body.appendChild(img);
     
     let header = document.getElementsByTagName('h4')[0];
     header.innerText=result.data.name;
+
+   
  
+
+
+ 
+    let high1 = document.getElementById("hightitle");
+    high1.innerText="High" ;
+
     let high = document.getElementsByClassName("card-title1")[0];
     high.innerText=Math.round(result.data.main.temp_max) + '\u00B0F' ;
     
+
+    let low1 = document.getElementById("lowtitle");
+    low1.innerText="Low";
 
     let low = document.getElementsByClassName("card-title2")[0];
     low.innerText=Math.round(result.data.main.temp_min) + '\u00B0F';
 
 
+    let forecast1 = document.getElementById("forcasttitle");
+    forecast1.innerText="Forcast";
+
     let forecast = document.getElementsByClassName("card-title3")[0];
     forecast.innerText=result.data.weather[0].description;
+
+    let humidity1 = document.getElementById("humiditytitle");
+    humidity1.innerText="Humidity";
 
     let humidity = document.getElementsByClassName("card-title4")[0];
     humidity.innerText=result.data.main.humidity + '%';
 }
 
+
+// function renderImage(){
+//     let image = document.getElementsByClassName('card')[0];
+//     let img = document.createElement("img")
+//     img.src = "images/clouds.jpg";
+//     img.class = "card-img img-fluid";
+//     img.alt = "...";
+//     image.appendChild(img);
     
+// }
+
+// renderImage()
